24699d0910530d38532c8856432c54e1b2fbc8fc
Rahul Sharma <rahuls2196@gmail.com>
2026-06-24 00:19:07 +0100

    build(webui): add Vite bundle entry

---
+++ webui/static/webui-build-entry.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import './scan-progress.css';
+import './ui-polish.css';
+import './launcher-controls.css';
+import './webui-enhancements.css';
+import './webui-core.js';
+import './webui-enhancements.js';
